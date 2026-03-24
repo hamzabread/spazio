98d5bf1ab9c215b6043c45d64d18fc3cd81c5f92
hamzabread <hamzaval2000@gmail.com>
2026-03-24 15:12:29 +0500

    landingfixes+about-page

--- assets/js/footerExternalLinks.js
+++ assets/js/footerExternalLinks.js
@@ -8,4 +8,31 @@ const footerExternalLinks = () => {
   });
 };
 
-export { footerExternalLinks };
+const cleanupRefParams = () => {
+  const domain = location.host.replace('www.', '');
+  const links = document.querySelectorAll('.gh-post-content a, .sitelinks-link a, .gh-sidebar-crypto-platforms a');
+  
+  if (!document.body.classList.contains('tag-hash-glossary')) {
+    links.forEach((link) => {
+      let href = link.href;
+      
+      // Remove ref tracking params
+      if (href.includes('?ref=www.spaziocrypto.com')) {
+        href = href.replace('?ref=www.spaziocrypto.com', '');
+      }
+      if (href.includes('&ref=www.spaziocrypto.com')) {
+        href = href.replace('&ref=www.spaziocrypto.com', '');
+      }
+      
+      // Open external links in new tab
+      if(!href.includes('www.spaziocrypto.com')){
+        link.setAttribute('target', '_blank');
+        link.setAttribute('rel', 'noreferrer noopener');
+      }
+      
+      link.href = href;
+    });
+  }
+};
+
+export { footerExternalLinks, cleanupRefParams };
